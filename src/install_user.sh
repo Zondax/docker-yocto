14093fc41ab5608590a1c97738f92d0277c9c91f
Juan Leni <lenijuan@gmail.com>
2021-11-29 02:38:06 +0100

    set user config

--- src/install_user.sh
+++ src/install_user.sh
@@ -6,3 +6,14 @@ set -e
 
 curl https://sh.rustup.rs -sSf | bash -s -- -y
 /tmp/install_rust.sh 1.54.0
+
+git config --global user.email "hello@zondax.ch"
+git config --global user.name "zondax"
+git config --global color.ui true
+
+ZSH_THEME=agnoster
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" ""  --unattended
+cd $HOME; \
+   git clone https://github.com/gpakosz/.tmux.git; \
+   ln -s -f .tmux/.tmux.conf ; \
+   cp .tmux/.tmux.conf.local .
